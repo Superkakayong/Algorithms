459d15ad1e7ccf4fdfedb412e7191a62decf06fc
Dayong Wu <dwu@fhcmi.org>
2023-05-13 22:34:01 -0400

    finished basic code of Linear Search

--- LienarSearch/src/LinearSearch.java
+++ LienarSearch/src/LinearSearch.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class LinearSearch {
+public class LinearSearch {
+    private LinearSearch() {}
+    public static int search(int[] data, int target) {
+        for (int i = 0; i < data.length; ++i) {
+            if (data[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {24, 18, 12, 9, 32, 4};
+
+        int res = LinearSearch.search(data, 122);
+        System.out.println(res);
+    }
 }
