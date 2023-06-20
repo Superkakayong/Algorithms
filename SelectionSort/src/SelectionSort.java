3ec62103bd28aa64244d7a568757071eccf5a313
Dayong Wu <dayong@bu.edu>
2023-06-19 21:09:49 -0400

    added generics and implemented Comparable interface

--- SelectionSort/src/SelectionSort.java
+++ SelectionSort/src/SelectionSort.java
@@ -1,13 +1,15 @@
+import java.util.Comparator;
+
 public class SelectionSort {
     private SelectionSort() {}
 
-    public static void sort(int[] arr) {
+    public static <E extends Comparable<E>> void sort(E[] arr) { // E must be comparable, so needs to extend Comparable interface
         for (int i = 0; i < arr.length; ++i) {
             // select the min value in [i, n)
             int minIndex = i;
 
             for (int j = i; j < arr.length; ++j) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
@@ -16,14 +18,14 @@ public class SelectionSort {
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
+    private static <E> void swap(E[] arr, int i, int j) {
+        E temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 4, 2, 3, 6, 5};
+        Integer[] arr = {1, 4, 2, 3, 6, 5};
 
         SelectionSort.sort(arr);
 
