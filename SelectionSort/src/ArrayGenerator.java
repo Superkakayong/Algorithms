b7535bce071bce0ee3f600fd56f2e7f6d5f791f2
Dayong Wu <dayong@bu.edu>
2023-08-09 07:41:45 -0400

    added a method generateRandomArray() to ArrayGenerator class

--- SelectionSort/src/ArrayGenerator.java
+++ SelectionSort/src/ArrayGenerator.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ArrayGenerator {
     private ArrayGenerator() {}
 
@@ -6,6 +8,18 @@ public class ArrayGenerator {
 
         for (int i = 0; i < n; ++i) {
             arr[i] = i;
+        }
+
+        return arr;
+    }
+
+    // generate an array with length n, each number is in the range of [0, bound)
+    public static Integer[] generateRandomArray(int n, int bound) {
+        Integer[] arr = new Integer[n];
+        Random seed = new Random();
+
+        for (int i = 0; i < n; ++i) {
+            arr[i] = seed.nextInt(bound);
         }
 
         return arr;
